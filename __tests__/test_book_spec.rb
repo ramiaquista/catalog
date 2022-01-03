3745415120729c8df5568be5b94e0d2b0e4b0197
enmanuel <enmanuellassis@gmail.com>
2022-01-03 18:42:54 -0400

    Add test for book and label methods

---
+++ __tests__/test_book_spec.rb
@@ -0,0 +1,15 @@
+require './book'
+require './item'
+
+describe Book do
+  context 'book class' do
+    it 'checks if the parent class is Item' do      
+      expect(Book.superclass).to equal(Item)
+    end
+    it 'should archived the book because the publish date was more than 10 years ago' do
+      book = Book.new('2000-01-04', 'castellana', 'good')
+      expect(book.can_be_archived?).to be true
+    end
+
+  end
+end
--- label.rb
+++ label.rb
@@ -10,6 +10,5 @@ class Label
 
   def add_item(item)
     @items << item
-    item.add_label(self)
   end
 end
