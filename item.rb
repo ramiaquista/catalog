8b956520e3926319d670932d8b7a62fc3eaae2cf
enmanuel <enmanuellassis@gmail.com>
2022-01-03 15:49:08 -0400

    Add methods to item class can_be_archived and move_to_archived

--- item.rb
+++ item.rb
@@ -1,11 +1,40 @@
+require 'date'
+
 class Item
-  def initialize()
+  attr_accessor :archived, :author, :source, :genre, :label
+
+  def initialize(publish_date, archived: false)
     @id = Random.rand(1..1000)
-    @genre = genre
-    @author = author
-    @source = source
-    @label = label
     @publish_date = publish_date
     @archived = archived
+  end
+
+  def add_genre(genre)
+    @genre = genre
+  end
+
+  def add_author(author)
+    @author = author
+  end
+
+  def add_source(source)
+    @source = source
+  end
+
+  def add_label(label)
+    @label = label
+  end
+
+  def can_be_archived?
+    date = Date.parse(@publish_date)
+    current_date = Time.now
+    return true if current_date.year - date.year >= 10
+
+    false
+  end
+
+  def move_to_archive
+    result = can_be_archived?
+    @archived = result
   end
 end
