1d06c37e6d3c88a1a1fc48eee506a7e15c61ff14
mustabbas2000@gmail.com <mustabbas2000@gmail.com>
2022-01-03 20:44:45 +0300

    Create Item class in a separate .rb file.

---
+++ item.rb
@@ -0,0 +1,11 @@
+class Item
+  def initialize()
+    @id = Random.rand(1..1000)
+    @genre = genre
+    @author = author
+    @source = source
+    @label = label
+    @publish_date = publish_date
+    @archived = archived
+  end
+end
