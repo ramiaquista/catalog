c8e3522b9bf5905da4311fcd2e91998321adcc2f
enmanuel <enmanuellassis@gmail.com>
2022-01-03 19:32:08 -0400

    Fix linters

--- __tests__/test_label_spec.rb
+++ __tests__/test_label_spec.rb
@@ -3,12 +3,12 @@ require './item'
 
 describe Label do
   it 'should return and empty array' do
-    label = Label.new('Label One','red')
+    label = Label.new('Label One', 'red')
     expect(label.items.length).to eq(0)
   end
-  
+
   it 'should return a length of one' do
-    label = Label.new('Label One','red')
+    label = Label.new('Label One', 'red')
     item = Item.new('2021-01-05')
     label.add_item(item)
     expect(label.items.length).to eq(1)
